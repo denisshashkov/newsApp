ab9289e72aa8863f34c1202f5eda7f366d659f01
denisshashkov <shashkov-56@mail.ru>
2021-08-19 16:51:39 +0300

    finished project

--- app.js
+++ app.js
@@ -58,11 +58,11 @@ const http = customHttp();
 
 const newsService = (function () {
   const apiKey = "15d584d03b1a4673be2274573f1790f1";
-  const apiUrl = "https://news-api-v2.herokuapp.com";
+  const apiUrl = "https://newsapi.org/v2";
   return {
-    topHeadlines(country = "us", cb) {
+    topHeadlines(country = "us", category = "general", cb) {
       http.get(
-        `${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,
+        `${apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`,
         cb
       );
     },
@@ -71,6 +71,16 @@ const newsService = (function () {
     },
   };
 })();
+
+//Elements
+const form = document.forms["newsControls"];
+const countrySelect = form.elements["country"];
+const categorySelect = form.elements["category"];
+const searchInput = form.elements["search"];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loadNews();
+});
 //  init selects
 document.addEventListener("DOMContentLoaded", function () {
   M.AutoInit();
@@ -79,17 +89,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //load news function
 function loadNews() {
-  newsService.topHeadlines("us", onGetResponse);
+  showLoader();
+  const country = countrySelect.value;
+  const category = categorySelect.value;
+  const searchText = searchInput.value;
+  if (!searchText) {
+    newsService.topHeadlines(country, category, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
 }
 
 //function on get response from server
 function onGetResponse(err, res) {
+  removeLoader();
+  if (err) {
+    showAlert(err, "error-msg");
+    return;
+  }
+  if (!res.articles.length) {
+    showAlert("no news on this category");
+    return;
+  }
   renderNews(res.articles);
 }
 
 //function render news
 function renderNews(news) {
   const newsContainer = document.querySelector(".news-container .row");
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
   let fragment = "";
   news.forEach((newsItem) => {
     const el = newsTemplate(newsItem);
@@ -98,13 +128,24 @@ function renderNews(news) {
   newsContainer.insertAdjacentHTML("afterbegin", fragment);
 }
 
+//function clear container
+function clearContainer(container) {
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
 //news item template
+
 function newsTemplate({ urlToImage, title, url, description }) {
+  let noImage = "http://placehold.it/350x200/99cccc.gif&text=The+image!";
   return `
     <div class="col s12">
       <div class ="card">
         <div class="card-image">
-          <img src="${urlToImage}">
+          <img src="${urlToImage || noImage}">
           <span class="card-title">${title || ""}</span>
         </div>
         <div class="card-content">
@@ -116,4 +157,34 @@ function newsTemplate({ urlToImage, title, url, description }) {
       </div>  
     </div>
   `;
+}
+
+function showAlert(msg, type = "success") {
+  M.toast({ html: msg, classes: "rounded" });
+}
+
+//function show loader
+function showLoader() {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="preloader-wrapper big active">
+       <div class="spinner-layer spinner-blue-only">
+        <div class="circle-clipper left">
+         <div class="circle"></div>
+       </div><div class="gap-patch">
+        <div class="circle"></div>
+       </div><div class="circle-clipper right">
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>`
+  );
+}
+
+//function remove loader
+function removeLoader() {
+  const loader = document.querySelector(".preloader-wrapper");
+  if (loader) {
+    loader.remove();
+  }
 }
